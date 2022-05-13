d5bef1117c20da3e053f172c20adbf661e06ca36
Luana Lessa <72531277+luanalessa@users.noreply.github.com>
2022-05-13 15:55:24 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
 import './index.css';
+
+import UserIcon from './assets/user.svg';
 
 function App() {
   return (
-    <div>
-      <h1>AAAAAAAAAAAAAA</h1>
-    </div>
+    <>
+      <header>
+        <div className="logo"></div>
+        <div className="menu">
+          <button className="btn first selected">Btn 01</button>
+          <button className="btn second">Btn 02</button>
+          <button className="btn third">Btn 03</button>
+          <button className="btn fourth">Btn 04</button>
+        </div>
+        <div className="user-profile">
+          <img src={UserIcon} alt="User Profil" />
+        </div>
+      </header>
+      <main></main>
+    </>
   );
 }
 
 export default App;
-
